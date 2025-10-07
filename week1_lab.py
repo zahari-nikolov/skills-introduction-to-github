c12d8d63e38ec14eb06370fa100435944107a93f
zahari-nikolov <26579553@edgehill.ac.uk>
2025-10-07 15:18:25 +0100

    Add files via upload

---
+++ week1_lab.py
@@ -0,0 +1,18 @@
+# Storing text (a string)
+student_name = "Zahari"
+
+# Stroring a whole number (an integer)
+student_age = 19
+
+# Print the variables to the console
+print("Student Name:", student_name)
+print("Student Age:", student_age)
+
+#A simple calculation
+num1 = 15
+num2 = 10
+total = num1 + num2
+
+print("Total:", total)
+
+
